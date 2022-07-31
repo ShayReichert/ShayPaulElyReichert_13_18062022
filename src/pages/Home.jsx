661a804e039a98f73c473389b0f582f630cde134
Shay Reichert <shay.reichert@outlook.fr>
2022-07-31 12:05:31 +0200

    Clean up

---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App">Coucou</div>;
+}
+
+export default App;
---
+++ src/utils/style/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primary: "#000",
+  secondary: "#000",
+  backgroundLight: "#FFF",
+  backgroundDark: "#000",
+  dark: "#000",
+};
+
+export default colors;
